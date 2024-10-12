0911774070f948a7cb4a01744a8e6ed66c25bc92
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2024-10-12 16:20:53 +0100

    create next prev buttons

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,27 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      if (newIndex > people.length - 1) {
+        return 0;
+      }
+      return newIndex;
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      if (newIndex < 0) {
+        return people.length - 1;
+      }
+      return newIndex;
+    });
+  };
+
   return (
     <main>
       <article className="review">
@@ -17,6 +38,14 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
       </article>
     </main>
   );
